d6d6023d32e94fd9ffe09c243d5afe5eaa811383
Rasbats <miker@members.swis.net>
2026-06-20 06:15:48 +0100

    Update test-format.cpp

--- test-format.cpp
+++ test-format.cpp
@@ -3,3 +3,9 @@ Using tab iso spaces
     float f = 1.f;
 long double ld = -.5L;
 double d = 1248e6;
+//
+float f = 1.0f;
+float f2 = 1.0;  // Also OK
+float f3 = 1;    // Also OK
+long double ld = -0.5L;
+double d = 1248.0e6;
